929e59f50186efd928b9d6c8f3ec32dda2f62b4a
Lennard Golsch <lennard.golsch@icloud.com>
2026-02-03 11:22:12 +0100

    improve project.js

--- src/main/resources/META-INF/resources/js/project.js
+++ src/main/resources/META-INF/resources/js/project.js
@@ -1,13 +1,11 @@
-
-$(document).ready(function() {
-
-  // spam protection for mails
-  $('span.madress').each(function(i) {
-      var text = $(this).text();
-      var address = text.replace(" [at] ", "@");
-      $(this).after('<a href="mailto:'+address+'">'+ address +'</a>')
-      $(this).remove();
+function replaceMaskedEmails() {
+  document.querySelectorAll("span.madress").forEach(span => {
+    const address = span.textContent.replace(" [at] ", "@");
+    const link = document.createElement("a");
+    link.href = `mailto:${address}`;
+    link.textContent = address;
+    span.replaceWith(link);
   });
+}
 
-});
-
+document.addEventListener("DOMContentLoaded", replaceMaskedEmails);
